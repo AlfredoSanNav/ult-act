183334870ecdc9dd3b5e3aacbcee03ea58640d60
Alfredo Sánchez Nava <54459482+AlfredoSanNav@users.noreply.github.com>
2021-11-24 18:52:36 -0600

    Add files via upload

---
+++ react-native-example-2021-master/react-native-example-2021-master/src/LoginView.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import {Actions} from 'react-native-router-flux'
+import Btn from './comp/simb/button';
+import TextField from './comp/simb/input';
+
+export default class LoginView extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      usuario :"",
+      contrasena:"",
+      emptyUsr: false,
+      emptyContrasena: false,
+    }
+  }
+  onChangeUser = (usuario) => this.setState({ usuario: usuario });
+  
+  onChangeContrasena = (contrasena) => this.setState({ contrasena: contrasena});
+  
+  isEmptyUsr = (bool) => this.setState({emptyUsr: bool});
+  isEmptyContrasena = (bool) => this.setState({emptyContrasena: bool});
+  getLogin = () => {
+    (this.state.usuario.trim() == "") ? this.isEmptyUsr(true):this.isEmptyUsr(false);
+    (this.state.contrasena.trim() == "") ? this.isEmptyContrasena(true):this.isEmptyContrasena(false);
+    if(this.state.usuario != "" && this.state.contrasena != ""){
+      Actions.home({user: this.state.usuario})
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={require('./music.jpg')}></Image>
+        <TextField placeholder={'Usuario'} secureTextEntry={false} isvalid={this.state.emptyUsr} textOnChange={this.onChangeUser}/>
+        <TextField placeholder={'Contraseña'} secureTextEntry={true} isvalid={this.state.emptyContrasena} textOnChange={this.onChangeContrasena}/>
+        <Btn title={'Login'} color={'#841584'} accessibilityLabelBtn={'Login button'} exeFunction={this.getLogin}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image:{
+    width: 200,
+    height: 200,
+    borderRadius: 10
+  }
+});
